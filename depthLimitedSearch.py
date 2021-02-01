a5ba976acaf55913f269fda26054ce6b9e805488
dhirajcodes <78296649+dhirajcodes@users.noreply.github.com>
2021-02-01 08:08:13 +0530

    Add files via upload

---
+++ depthLimitedSearch.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  1 05:36:35 2021
+
+@author: deepak
+"""
+
+from collections import defaultdict
+
+class Graph:
+    def __init__(self,vertices):
+        self.V=vertices
+        self.graph=defaultdict(list)
+        
+    def addEdge(self,u,v):
+        self.graph[u].append(v)
+        
+    def DLS(self,source,target,maxDepth):
+        if source==target:
+            return True
+        if maxDepth<=0 :
+            return False
+        for i in self.graph[source]:
+            if(self.DLS(i, target, maxDepth-1)):
+                return True
+        return False
+
+g=Graph(9)
+g.addEdge(0,1)
+g.addEdge(0,2)
+g.addEdge(1,3)
+g.addEdge(1,4)
+g.addEdge(2,5)
+g.addEdge(2,6)
+g.addEdge(3,7)
+g.addEdge(3,8)
+
+target=3
+maxDepth=3
+source=0    
+
+
+if g.DLS(source,target,maxDepth)==True:
+    print("Target ",target,"is reachable from Source ",source," within max depth ",maxDepth)
+else:
+    print("Target ",target,"is NOT reachable from Source ",source," within max depth ",maxDepth)
+     
+       
---
+++ minmax.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  1 05:52:27 2021
+
+@author: deepak
+"""
+
+import math
+import random
+
+def minmax(currentDepth,nodeIndex,maxTurn,score,targetDepth):
+    if(currentDepth==targetDepth):
+        return score[nodeIndex]
+    if(maxTurn):
+        return max(minmax(currentDepth+1,nodeIndex*2,False,score,targetDepth),minmax(currentDepth+1,nodeIndex*2+1,False,score,targetDepth))
+    else:
+        return min(minmax(currentDepth+1,nodeIndex*2,True,score,targetDepth),minmax(currentDepth+1,nodeIndex*2+1,True,score,targetDepth))
+    
+score=random.sample(range(1,50),4)
+print(str(score))
+treeDepth=math.log(len(score),2)
+print("The optimal value is: ",end="")
+print(minmax(0,0,True,score,treeDepth))   
